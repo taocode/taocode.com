21c089cec2f9b3043189504132fcf00809766565
Mark Jones <mark@taocode.com>
2021-06-04 17:55:02 -0400

    Added aria-label for Back To Top button

--- src/lib/ToTop.svelte
+++ src/lib/ToTop.svelte
@@ -8,6 +8,7 @@
 
 <Headroom bottom={true}>
   <div 
-  class="pr-6 pb-6 md:pr-10 lg:pr-24 xl:pr-48 text-right mx-auto max-w-screen-xl"><button class="btn-primary rounded shadow-totop inline-block pb-2 pt-0 px-2" on:click={()=>{ window.scroll(0,0) }}><Icon data={chevronUp}/></button></div>
+  class="pr-6 pb-6 md:pr-10 lg:pr-24 xl:pr-48 text-right mx-auto max-w-screen-xl">
+  <button aria-label="back to top" class="btn-primary rounded shadow-totop inline-block pb-2 pt-0 px-2" on:click={()=>{ window.scroll(0,0) }}><Icon data={chevronUp}/></button></div>
 </Headroom>
 
