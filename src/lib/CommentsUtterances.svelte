a140548525c9af78ae7319971d522407cf8ef4c9
Mark Jones <mark@taocode.com>
2021-06-09 16:46:18 -0400

    Upgraded comments to use dark or light theme based on (prefers-color-scheme: ...) media query.

--- src/lib/CommentsUtterances.svelte
+++ src/lib/CommentsUtterances.svelte
@@ -1,17 +1,17 @@
 <script>
-import { onMount } from 'svelte';
+import { onMount } from 'svelte'
 
 onMount(() => {
-    var s = document.createElement('script'),
-    tag = document.getElementById('utterances');
-    s.setAttribute('repo',"taocode/taocode.com");
-    s.setAttribute('issue-term',"pathname");
-    s.setAttribute('label',"✨💬");
-    s.setAttribute('theme',"github-dark");
-    s.setAttribute('crossorigin',"anonymous");
-
-    s.src = 'https://utteranc.es/client.js';
-    tag.parentNode.insertBefore(s, tag);
-});
+    const darkMode = ! matchMedia('(prefers-color-scheme: light)').matches
+    const s = document.createElement('script')
+    const tag = document.getElementById('utterances')
+    s.setAttribute('repo',"taocode/taocode.com")
+    s.setAttribute('issue-term',"pathname")
+    s.setAttribute('label',"✨💬")
+    s.setAttribute('theme',"github-"+(darkMode)?"dark":"light")
+    s.setAttribute('crossorigin',"anonymous")
+    s.src = 'https://utteranc.es/client.js'
+    tag.parentNode.insertBefore(s, tag)
+})
 </script>
 <div id="utterances"></div>
