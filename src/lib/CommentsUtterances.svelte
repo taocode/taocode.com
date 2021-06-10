0481ea839a4f94cd7cab24f39954f9ae4822649b
Mark Jones <mark@taocode.com>
2021-06-09 22:05:42 -0400

    Update for utterances to use preferred-color-scheme

--- src/lib/CommentsUtterances.svelte
+++ src/lib/CommentsUtterances.svelte
@@ -2,13 +2,12 @@
 import { onMount } from 'svelte'
 
 onMount(() => {
-    const darkMode = ! matchMedia('(prefers-color-scheme: light)').matches
     const s = document.createElement('script')
     const tag = document.getElementById('utterances')
     s.setAttribute('repo',"taocode/taocode.com")
     s.setAttribute('issue-term',"pathname")
     s.setAttribute('label',"✨💬")
-    s.setAttribute('theme',"github-"+(darkMode)?"dark":"light")
+    s.setAttribute('theme',"preferred-color-scheme")
     s.setAttribute('crossorigin',"anonymous")
     s.src = 'https://utteranc.es/client.js'
     tag.parentNode.insertBefore(s, tag)
