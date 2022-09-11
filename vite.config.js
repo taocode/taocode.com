989854e461885f487380a708cd0722bdd95bffc2
taocode <mark@taocode.com>
2022-09-10 20:54:55 -0400

    feat: added autoimport

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { imagetools } from 'vite-imagetools'
 import WindiCSS from 'vite-plugin-windicss'
+import autoImport from 'sveltekit-autoimport'
 
 export default {
   plugins: [
+    autoImport({
+      components: ['./src/lib/components'],
+      // some frequently used modules
+      module: {
+        svelte: ['onMount', 'createEventDispatcher'],
+        '$app/environment': ['browser','dev'],
+      },
+      include: ['**/*.svelte','./src/content/blog/**/*.md'],
+    }),
     WindiCSS(),
     imagetools({ force: true }),
     sveltekit(),
