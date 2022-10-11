83963dae8dadd368324646f46c7cec297ccef2f0
taocode <mark@taocode.com>
2022-10-11 04:59:46 -0400

    fix: simplified display of previous/next article links

--- src/lib/components/blog/PrevNextArticle.svelte
+++ src/lib/components/blog/PrevNextArticle.svelte
@@ -1,47 +1,56 @@
 <script lang="ts">
-  import type { Post } from '$lib/models/post';
+  import type { Post } from '$lib/models/post'
 
-  export let previousArticle: Post;
-  export let nextArticle: Post;
+  export let previousArticle: Post
+  export let nextArticle: Post
+
 </script>
 
 <div class="font-display flex flex-wrap mb-10 -m-2">
   {#if previousArticle}
-    <div class="w-full p-2 md:w-1/2">
-      <div class="h-full p-5 card bg-gray-light">
-        <small class="block text-sm text-gray">
-          <em>Previous article</em>
-        </small>
-        <b>
-          <a
-            data-sveltekit-prefetch
-            href="/blog/{previousArticle.slug}"
-            class="text-xl no-underline hover:underline link-inherit"
-            style="font-weight: 700;"
-          >
-            {previousArticle.title}
-          </a>
-        </b>
+    <div class="neighbor previous">
+      <div class="card">
+        <a data-sveltekit-prefetch href="/blog/{previousArticle.slug}">
+          {previousArticle.title}
+        </a>
       </div>
     </div>
   {/if}
   {#if nextArticle}
-    <div class="w-full p-2 md:w-1/2">
-      <div class="h-full p-5 card bg-gray-light">
-        <small class="block text-sm text-gray">
-          <em>Next article</em>
-        </small>
-        <b>
-          <a
-            data-sveltekit-prefetch
-            href="/blog/{nextArticle.slug}"
-            class="text-xl no-underline hover:underline link-inherit"
-            style="font-weight: 700;"
-          >
+    <div class="neighbor next">
+      <div class="card">
+          <a data-sveltekit-prefetch href="/blog/{nextArticle.slug}">
             {nextArticle.title}
           </a>
-        </b>
       </div>
     </div>
   {/if}
 </div>
+
+<style lang="postcss">
+.neighbor {
+  @apply w-full p-2 md:w-1/2;
+  &.previous {
+    --label: 'Previous article';
+  }
+  &.next {
+    --label: 'Next article';
+  }
+  .card {
+    @apply relative flex h-full
+      bg-gray-300 border-gray-400
+      dark:(bg-gray-900 border-gray-800);
+    &:hover {
+      @apply border-gray-700;
+    }
+    &::before {
+      content: var(--label);
+      @apply absolute z-0 p-5 block text-sm text-gray-600 italic dark:text-gray-500;
+    }
+  }
+  a {
+    @apply z-10 block p-5 pt-12 text-xl no-underline font-bold text-[inherit] hover:(underline text-[inherit]);
+  }
+}
+
+</style>
