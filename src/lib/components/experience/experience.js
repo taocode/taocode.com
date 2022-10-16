c1c091693cb68168b691fee9aa147cceb2f55378
taocode <mark@taocode.com>
2022-10-15 22:58:14 -0400

    fix: experience chart updates to years of experience

--- src/lib/components/experience/experience.js
+++ src/lib/components/experience/experience.js
@@ -1,7 +1,7 @@
 export default [
   { name: 'CMS', years: 16,
     children: [
-      { name: 'Hugo & other SSG (11ty, etc)', years: 1.6 },
+      { name: 'Hugo & other SSG (11ty, etc)', years: 2 },
       { name: 'WordPress', years: 2},
       { name: 'ConcreteCMS', years: 2.5 },
       { name: 'Drupal', years: 5.5},
@@ -9,7 +9,7 @@ export default [
 	},
   { name: 'CSS', years: 19,
 		children: [
-			{ name: 'Tailwind/Windi', years: 0.6 },
+			{ name: 'Tailwind/Windi', years: 1 },
 			{ name: 'Flex', years: 4}, 
 			{ name: 'SASS/SCSS/LESS', years: 6}, 
 			{ name: 'CSS3', years: 7}, 
@@ -17,8 +17,8 @@ export default [
 	},
   { name: 'JS', years: 22,
     children: [
-			{ name: 'Svelte', years: 0.4 },
-			{ name: 'NodeJS', years: 2}, 
+			{ name: 'Svelte', years: 1.2 },
+			{ name: 'NodeJS', years: 2 }, 
 			{ name: 'jQuery', years: 5}, 
 			{ name: 'Vanilla', years: 10}, 
 		]
