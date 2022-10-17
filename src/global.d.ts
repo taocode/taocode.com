1e2ab3995ee9cb8966584f5ed035124fa7ea7b98
taocode <mark@taocode.com>
2022-10-17 17:49:20 -0400

    fix: removed flatten

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,7 +3,6 @@
 /// <reference types="vite/client" />
 
 // added stub declarations for these deps to silence svelte-check warnings
-declare module 'flatten';
 declare module 'svelte-click-outside';
 declare module 'svelte-forms-lib';
 declare module 'svelte-awesome';
