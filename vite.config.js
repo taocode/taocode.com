e707ef750469e6efc29da063bd83138c7fd614ac
taocode <mark@taocode.com>
2023-02-11 09:57:54 -0500

    fix(uno): moved unocss icons ahead of sveltekit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { imagetools } from 'vite-imagetools'
 import WindiCSS from 'vite-plugin-windicss'
-import unocss from "unocss/vite"
+import unocss from 'unocss/vite'
 import unoConfig from './uno.config'
 
 export default {
   plugins: [
+    unocss(unoConfig),
     sveltekit(),
     WindiCSS(),
-    unocss(unoConfig),
     imagetools({ force: true }),
   ],
 }
